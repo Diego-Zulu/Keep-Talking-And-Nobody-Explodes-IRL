1dea3d67fe29937c1b182a689da16c14e1dd8304
Unknown <santi17mendez@hotmail.com>
2017-10-23 17:48:06 -0300

    Auto stash before merge of "develop" and "origin/develop"

---
+++ Arduino/Wires/Wires.ino
@@ -0,0 +1,70 @@
+#include <LinkedList.h>
+
+#define FIRST_WIRE 5
+#define SECOND_WIRE 4
+#define THIRD_WIRE 0
+#define FOURTH_WIRE 2
+#define FIFTH_WIRE 14
+#define SIXTH_WIRE 12
+
+#define LAST_WIRE FIRST_WIRE+5
+#define VICTORY_LED 13
+#define TOTAL_WIRES 6
+
+int wiresPos[] = {FIRST_WIRE, SECOND_WIRE, THIRD_WIRE, FOURTH_WIRE, FIFTH_WIRE, SIXTH_WIRE};
+
+int wires_amount;
+int correct_wire_count;
+int correct_wire;
+bool won;
+bool winsThisRound;
+bool errorThisRound;
+LinkedList<int> *wiresActive = new LinkedList<int>();
+
+void setup() {
+  Serial.begin(9600);
+  
+  pinMode(VICTORY_LED, OUTPUT);
+  won = false;
+  int correct_wire_count = 2;
+  int correct_wire = FIRST_WIRE;
+
+  for (int i = 0; i <= TOTAL_WIRES && wiresActive->size() < wires_amount; i++) {
+    pinMode(wiresPos[i], INPUT_PULLUP);
+    if (digitalRead(wiresPos[i]) == LOW) {
+      wiresActive->add(wiresPos[i]);
+      if (correct_wire_count == 0) {
+        correct_wire = wiresPos[i];
+      }
+      correct_wire_count--;
+    }
+  }
+
+}
+
+void loop() {
+  if (!won) {
+    winsThisRound = false;
+    errorThisRound = false;
+
+    for (int i = 0; i < wiresActive->size() && !errorThisRound; i++) {
+      Serial.println(wiresActive->get(i));
+      if (digitalRead(wiresActive->get(i)) == HIGH) {
+        if (correct_wire == wiresActive->get(i)) {
+          winsThisRound = true;
+        } else {
+          errorThisRound = true;
+          wiresActive->remove(i);
+          i--;
+        } 
+      }
+    }
+
+    if (winsThisRound) {
+      digitalWrite(VICTORY_LED, HIGH);
+      won = true;
+    } else if (errorThisRound) {
+      
+    }
+  }
+}
---
+++ Arduino/libraries/ClientMQTT/examples/ClientBasic/ClientBasic.ino
@@ -0,0 +1,46 @@
+#include <ClientMQTT.h>
+
+const char* ssid = "WIFI_NAME";
+const char* password = "WIFI_PASS";
+const char* mqtt_server = "IP_SERVER_MQTT";
+const unsigned int port = 1883;
+const char* module = "Wires";
+ClientMQTT client(module);
+
+void f_Start(char* message) {
+  Serial.println("HELLI: START");
+}
+
+void f_End(char* message) {
+  Serial.println("HELLI: END");
+  started = false;
+}
+
+void f_OnMessage(char* message) {
+  Serial.println("HELLI: ON MESSAGE");
+}
+
+void setup() {
+  Serial.begin(9600);
+  client.connectWIFI(ssid, password);
+  client.connectMQTT(mqtt_server, port, f_Start, f_End, f_OnMessage);
+}
+
+void loop() {
+  if(!client.isListening()) {
+    client.startListeningLoop();
+  }
+  client.refresh();
+  delay(1000);
+  /*
+  Serial.println("WITH WAITING");
+  client.sendMessage("0");
+  client.Loop();
+  */
+  Serial.println("WITHOUT WAITING");
+  char* response = client.sendMessageAndWaitForResponse("1");
+  Serial.print("RESPONSE: ");
+  Serial.println(response);
+  delete response;
+  /**/
+}
