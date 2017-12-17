2bf98c1227dbff4ebdc3a3b794cfaacea8b15186
Diego Zuluaga <diego_i_zuluaga@outlook.com>
2017-12-17 16:20:01 -0300

    Finished Simons Says MQTT

--- Arduino/Simon_Dice_MQTT/Simon_Dice_MQTT.ino
+++ Arduino/Simon_Dice_MQTT/Simon_Dice_MQTT.ino
@@ -7,31 +7,25 @@
 #define CHOICE_BLUE (1 << 2)
 #define CHOICE_YELLOW   (1 << 3)
 
-#define LED_RED     13
-#define LED_GREEN   5
-#define LED_BLUE    14
-#define LED_YELLOW  0
-#define VICTORY_LED  3
+#define LED_RED     9
+#define LED_GREEN   7
+#define LED_BLUE    5
+#define LED_YELLOW  3
+#define BIG_GREEN  2
 
-#define BUTTON_RED    15
-#define BUTTON_GREEN  4
-#define BUTTON_BLUE   12
-#define BUTTON_YELLOW 2
+// Button pin definitions
+#define BUTTON_RED    10
+#define BUTTON_GREEN  8
+#define BUTTON_BLUE   6
+#define BUTTON_YELLOW 4
 
-#define ROUNDS_TO_WIN      32 
-#define ENTRY_TIME_LIMIT   3000 
+#define ENTRY_TIME_LIMIT   3000 //Amount of time to press a button before game times out. 3000ms = 3 sec
 
-#define MODE_MEMORY  0
-#define MODE_BATTLE  1
-#define MODE_BEEGEES 2
-
-string gameBoard[32]; 
+byte gameBoard[32]; //Contains the combination of buttons as we advance
 int gameBoardLength = 32;
 string userInputs[32];
-byte gameRound = 0; 
-byte currentMove = 0;
+byte gameRound = 0; //Counts the number of succesful rounds the player has made it through
 bool won = false;
-bool waitingForResponse = false;
 
 const char* ssid = "ASUS";
 const char* password = "M#V#23622607";
@@ -40,7 +34,7 @@ const unsigned int port = 1883;
 const char* module = "SimonSays";
 ClientMQTT client(module);
 
-void sendInputsToServer() {
+bool sendInputsToServerAndCheckIfOkay(int currentMove) {
 
   waitingForResponse = true;
   string s = "";
@@ -50,8 +44,8 @@ void sendInputsToServer() {
   s += userInputs[currentMove];
   Serial.println("SEND");
   Serial.println(s.c_str());
-  client.sendMessage(s.c_str());
-  client.refresh();
+  char* message = client.sendMessageAndWaitForResponse(s.c_str());
+  return strcmp(message, "OK") == 0
 }
 
 int countCommasInMessage(char* message) {
@@ -65,7 +59,7 @@ int countCommasInMessage(char* message) {
 
   }
   
-  return ++count;
+  return count;
 }
 
 void fillGameBoardWithColors(char* message) {
@@ -75,7 +69,10 @@ void fillGameBoardWithColors(char* message) {
   for(int i = 0; message[i] != '\0' && wordsExtracted <= gameBoardLength; i++) {
 
     if (message[i] == ',') {
-      gameBoard[wordsExtracted] = s;
+       if(s == "R") gameBoard[wordsExtracted] = CHOICE_RED;
+          else if(s == "G") gameBoard[wordsExtracted] = CHOICE_GREEN;
+          else if(s == "BLU") gameBoard[wordsExtracted] = CHOICE_BLUE;
+          else if(s == "Y") gameBoard[wordsExtracted] = CHOICE_YELLOW;
       wordsExtracted++;
       s = "";
     } else {
@@ -90,7 +87,7 @@ void f_Start(char* message) {
  Serial.begin(9600);
    Serial.println("FSTART");
  Serial.println(message);
-  int gameBoardLength = countCommasInMessage(message);
+  int gameBoardLength = countCommasInMessage(message) + 1;
   fillGameBoardWithColors(message);
   
   won = false;
@@ -99,22 +96,12 @@ void f_Start(char* message) {
 
 void f_End(char* message) {
   won = true;
+  digitalWrite(VICTORY_LED, LOW);
   Serial.println("FEND");
 }
 
 void f_OnMessage(char* message) {
   Serial.println("ONMESSAGE");
-  if (strcmp(message, "OK") == 0) {
-    gameRound++;
-    if (gameRound >= gameBoardLength) {
-      digitalWrite(VICTORY_LED, HIGH);
-      won = true;
-    }
-
-  } else if (strcmp(message, "ERROR") == 0) {
-   //clean userInputs
-   gameRound = 0;
-  }
 }
 
 void setup()
@@ -145,53 +132,76 @@ void loop()
   if(!client.isListening()) {
     client.startListeningLoop();
   }
+  client.refresh();
 
-    if (won) {
-      play_winner(); 
-      } else {
-        play_memory();
-      }
+  setLEDs(CHOICE_RED | CHOICE_GREEN | CHOICE_BLUE | CHOICE_YELLOW); 
+  delay(1000);
+  setLEDs(CHOICE_OFF); 
+  delay(250);
+
+    if (!won && play_memory() == true) 
+      play_winner(); // Player won, play winner tones
 }
 
 boolean play_memory(void)
 {
 
-  gameRound = 0;
+  gameRound = 0; // Reset the game to the beginning
 
-  while (!won) 
+  while (gameRound < gameBoardLength) 
   {
-    Serial.println("LOOP");
-    client.refresh();
-    playMoves();
+    gameRound++;
 
-    for (currentMove = 0 ; currentMove <= gameRound ; currentMove++)
+    playMoves(); // Play back the current game board
+
+    // Then require the player to repeat the sequence.
+    for (byte currentMove = 0 ; currentMove < gameRound ; currentMove++)
     {
+      client.refresh();
+
       byte choice = wait_for_button(); // See what button the user presses
 
       if (choice == 0) {
         currentMove--; 
         playMoves();
       } else {
+         //if (choice != correctInputs[currentMove]) return false; // If the choice is incorect, player loses
+        
          if(choice == CHOICE_RED) userInputs[currentMove] = "R";
           else if(choice == CHOICE_GREEN) userInputs[currentMove] = "G";
           else if(choice == CHOICE_BLUE) userInputs[currentMove] = "BLU";
           else if(choice == CHOICE_YELLOW) userInputs[currentMove] = "Y";
 
-          
-          sendInputsToServer();
-          while (waitingForResponse) {
-            client.refresh();
+          if (!sendInputsToServerAndCheckIfOkay(currentMove)) {
+            return false;
           }
-          
       }
     }
 
-    delay(1000);
+    delay(1000); // Player was correct, delay before playing moves
   }
 
-  return true;
+  return true; // Player made it through all the rounds to win!
 }
 
+// Plays the current contents of the game moves
+void playMoves(void)
+{
+  for (byte currentMove = 0 ; currentMove < gameRound ; currentMove++) 
+  {
+    toner(gameBoard[currentMove], 150);
+
+    // Wait some amount of time between button playback
+    // Shorten this to make game harder
+    delay(150); // 150 works well. 75 gets fast.
+  }
+}
+
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+//The following functions control the hardware
+
+// Lights a given LEDs
+// Pass in a byte that is made up from CHOICE_RED, CHOICE_YELLOW, etc
 void setLEDs(byte leds)
 {
   if ((leds & CHOICE_RED) != 0)
@@ -215,38 +225,8 @@ void setLEDs(byte leds)
     digitalWrite(LED_YELLOW, LOW);
 }
 
-byte transformMoveIntoByte(int i) {
-
-  if (gameBoard[i] == "R") {
-    return CHOICE_RED;
-  } else if (gameBoard[i] == "BLU") {
-    return CHOICE_BLUE;
-  } else if (gameBoard[i] == "G") {
-    return CHOICE_GREEN;
-  } else if (gameBoard[i] == "Y") {
-    return CHOICE_YELLOW;
-  }
-
-  return 0;
-}
-
-void playMoves(void)
-{
-
-  for (int currentMove = 0; currentMove <= gameRound ; currentMove++) 
-  {
-    byte l = transformMoveIntoByte(currentMove);
-      setLEDs(l); //Turn on a given LED
-
-  delay(150);
-
-  setLEDs(CHOICE_OFF);
-
-   delay(150);
-  }
-}
-
-
+// Wait for a button to be pressed. 
+// Returns one of LED colors (LED_RED, etc.) if successful, 0 if timed out
 byte wait_for_button(void)
 {
   long startTime = millis(); // Remember the time we started the this loop
@@ -257,18 +237,21 @@ byte wait_for_button(void)
 
     if (button != CHOICE_NONE)
     { 
-      while(checkButton() != CHOICE_NONE) ; 
+      toner(button, 150); // Play the button the user just pressed
 
-      delay(10); 
+      while(checkButton() != CHOICE_NONE) ;  // Now let's wait for user to release button
+
+      delay(10); // This helps with debouncing and accidental double taps
 
       return button;
     }
 
   }
 
-  return CHOICE_NONE;
+  return CHOICE_NONE; // If we get here, we've timed out!
 }
 
+// Returns a '1' bit in the position corresponding to CHOICE_RED, CHOICE_GREEN, etc.
 byte checkButton(void)
 {
   if (digitalRead(BUTTON_RED) == 0) return(CHOICE_RED); 
@@ -279,8 +262,131 @@ byte checkButton(void)
   return(CHOICE_NONE); // If no button is pressed, return none
 }
 
+// Light an LED and play tone
+// Red, upper left:     440Hz - 2.272ms - 1.136ms pulse
+// Green, upper right:  880Hz - 1.136ms - 0.568ms pulse
+// Blue, lower left:    587.33Hz - 1.702ms - 0.851ms pulse
+// Yellow, lower right: 784Hz - 1.276ms - 0.638ms pulse
+void toner(byte which, int buzz_length_ms)
+{
+  setLEDs(which); //Turn on a given LED
+
+  //Play the sound associated with the given LED
+  switch(which) 
+  {
+  case CHOICE_RED:
+    buzz_sound(buzz_length_ms, 1136); 
+    break;
+  case CHOICE_GREEN:
+    buzz_sound(buzz_length_ms, 568); 
+    break;
+  case CHOICE_BLUE:
+    buzz_sound(buzz_length_ms, 851); 
+    break;
+  case CHOICE_YELLOW:
+    buzz_sound(buzz_length_ms, 638); 
+    break;
+  }
+
+  setLEDs(CHOICE_OFF); // Turn off all LEDs
+}
+
+// Toggle buzzer every buzz_delay_us, for a duration of buzz_length_ms.
+void buzz_sound(int buzz_length_ms, int buzz_delay_us)
+{
+  // Convert total play time from milliseconds to microseconds
+  long buzz_length_us = buzz_length_ms * (long)1000;
+
+  // Loop until the remaining play time is less than a single buzz_delay_us
+  while (buzz_length_us > (buzz_delay_us * 2))
+  {
+    buzz_length_us -= buzz_delay_us * 2; //Decrease the remaining play time
+
+    //digitalWrite(BUZZER1, LOW);
+    //digitalWrite(BUZZER2, HIGH);
+    delayMicroseconds(buzz_delay_us);
+
+    //digitalWrite(BUZZER1, HIGH);
+    //digitalWrite(BUZZER2, LOW);
+    delayMicroseconds(buzz_delay_us);
+  }
+}
+
+// Play the winner sound and lights
 void play_winner(void)
 {
-  digitalWrite(VICTORY_LED, HIGH);
+  digitalWrite(BIG_GREEN, HIGH);
   won = true;
+}
+
+// Play the winner sound
+// This is just a unique (annoying) sound we came up with, there is no magic to it
+void winner_sound(void)
+{
+  // Toggle the buzzer at various speeds
+  for (byte x = 250 ; x > 70 ; x--)
+  {
+    for (byte y = 0 ; y < 3 ; y++)
+    {
+      //digitalWrite(BUZZER2, HIGH);
+      //digitalWrite(BUZZER1, LOW);
+      delayMicroseconds(x);
+
+      //digitalWrite(BUZZER2, LOW);
+      //digitalWrite(BUZZER1, HIGH);
+      delayMicroseconds(x);
+    }
+  }
+}
+
+// Play the loser sound/lights
+void play_loser(void)
+{
+ delay(1000);
+}
+
+int noteDuration = 115; // This essentially sets the tempo, 115 is just about right for a disco groove :)
+int LEDnumber = 0; // Keeps track of which LED we are on during the beegees loop
+
+// Do nothing but play bad beegees music
+// This function is activated when user holds bottom right button during power up
+void play_beegees()
+{
+  //Turn on the bottom right (yellow) LED
+  setLEDs(CHOICE_YELLOW);
+  toner(CHOICE_YELLOW, 150);
+
+  setLEDs(CHOICE_RED | CHOICE_GREEN | CHOICE_BLUE); // Turn on the other LEDs until you release button
+
+  while(checkButton() != CHOICE_NONE) ; // Wait for user to stop pressing button
+
+  setLEDs(CHOICE_NONE); // Turn off LEDs
+
+  delay(1000); // Wait a second before playing song
+
+  //digitalWrite(BUZZER1, LOW); // setup the "BUZZER1" side of the buzzer to stay low, while we play the tone on the other pin.
+
+  while(checkButton() == CHOICE_NONE) //Play song until you press a button
+  {
+    // iterate over the notes of the melody:
+    for (int thisNote = 0; thisNote < 32; thisNote++) {
+      changeLED();
+      //tone(BUZZER2, melody[thisNote],noteDuration);
+      // to distinguish the notes, set a minimum time between them.
+      // the note's duration + 30% seems to work well:
+      int pauseBetweenNotes = noteDuration * 1.30;
+      delay(pauseBetweenNotes);
+      // stop the tone playing:
+      //noTone(BUZZER2);
+    }
+  }
+}
+
+// Each time this function is called the board moves to the next LED
+void changeLED(void)
+{
+  setLEDs(1 << LEDnumber); // Change the LED
+
+  LEDnumber++; // Goto the next LED
+  if(LEDnumber > 3) LEDnumber = 0; // Wrap the counter if needed
 }
